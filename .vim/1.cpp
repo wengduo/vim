d17da9ad01e7e82b98c991fea26981470392b1fd
wengduo <2292466904@qq.com>
2017-05-05 12:21:02 +0800

    vim配置

---
+++ .vim/1.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+    > File Name: 1.cpp
+    > Author: wengduo
+    > Mail: 2292466904@qq.com
+    > Created Time: 2016年08月22日 星期一 07时08分23秒
+ ************************************************************************/
+
+#include <iostream>
+#include <cstdio>
+#include <cassert>
+
+int main(int argc,char *argv[])
+{
+	printf("hel\n")	
+	return 0;
+}
